293ac39307207e8e4563f2b5fabb7baaece9e786
tzejian <akernus.lr@gmail.com>
2017-05-23 15:17:44 +0800

    song class file

---
+++ app/src/main/java/com/example/potran/p05_songsofsingapore/Song.java
@@ -0,0 +1,48 @@
+package com.example.potran.p05_songsofsingapore;
+
+import android.database.sqlite.SQLiteDatabase;
+
+import java.io.Serializable;
+
+/**
+ * Created by 127107 on 23/5/2017.
+ */
+
+public class Song implements Serializable {
+    private int _id;
+    private String title;
+    private String singers;
+    private int year;
+    private int stars;
+
+    public int get_id() {
+        return _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSingers() {
+        return singers;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getStars() {
+        return stars;
+    }
+
+    public Song(String title, String singers, int _id, int year, int stars){
+        this.title = title;
+        this.singers = singers;
+        this._id = _id;
+        this.stars = stars;
+        this.year = year;
+
+    }
+
+
+}
